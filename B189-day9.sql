dbeef3a506867457f9cb0dd71f6687de413c85af
mfntozcan@gmail.com <mfntozcan@gmail.com>
2023-10-13 22:50:04 +0200

    'day9'

--- B189-day9.sql
+++ B189-day9.sql
@@ -234,6 +234,73 @@ ALTER COLUMN isim DROP NOT NULL
 --		ROLLBACK; yapilan degisiklikleri iptal ederek en son kalici hale getirilen konuma geri doner ve transactionu sonlandirir.
 
 
+CREATE TABLE hesaplar
+(
+hesap_no int UNIQUE,
+isim VARCHAR(50),
+bakiye real       
+);
+
+INSERT INTO hesaplar VALUES(1234,'Harry Potter',10000.3);
+INSERT INTO hesaplar VALUES(5678,'Jack Sparrow',5000.5);
+
+
+SELECT * FROM hesaplar;
+
+
+UPDATE hesaplar SET bakiye=bakiye-1000 WHERE hesap_no=1234;
+--sistemde hata oluştu
+UPDATE hesaplar SET bakiye=bakiye+1000 WHERE hesap_no=5678;
+--veriler tutarsız
+
+SELECT * FROM hesaplar;
+
+-------------------------------------------------
+
+BEGIN;
+CREATE TABLE hesaplar
+(
+hesap_no int UNIQUE,
+isim VARCHAR(50),
+bakiye real       
+);
+COMMIT;
+
+BEGIN;
+INSERT INTO hesaplar VALUES(1234,'Harry Potter',10000.3);
+INSERT INTO hesaplar VALUES(5678,'Jack Sparrow',5000.5);
+
+SAVEPOINT x;
+
+SELECT * FROM hesaplar;
+
+--try{
+UPDATE hesaplar SET bakiye=bakiye-1000 WHERE hesap_no=1234;
+
+--sistemde hata oluştu, catch bloğundan devam
+
+UPDATE hesaplar SET bakiye=bakiye+1000 WHERE hesap_no=5678;--bu işlem başarısız
+--veriler tutarsız
+
+COMMIT;
+--}catch(hata){
+ROLLBACK TO x;
+--}
+
+-----------------------------------pozitif senaryo
+--try{
+UPDATE hesaplar SET bakiye=bakiye-1000 WHERE hesap_no=1234;--başarılı
+
+UPDATE hesaplar SET bakiye=bakiye+1000 WHERE hesap_no=5678;--başarılı
+
+COMMIT;
+
+--}catch(hata){
+ROLLBACK TO x;
+--}
+
+SELECT * FROM hesaplar;
+
 
 
 
